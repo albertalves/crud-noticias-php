dc5c9bd6b22c8d3b2d88a708c906e97287f9e6f0
albertalves <31518040+albertalves@users.noreply.github.com>
2020-12-07 20:27:17 -0300

    .

--- layout/footer.php
+++ layout/footer.php
@@ -3,4 +3,30 @@
             <p>Desenvolvido por albert alves</p>
         </footer>
 	</body>
+	<script>
+	function getIp(callback){
+	    function response(s){
+		callback(window.userip);
+		s.onload = s.onerror = null;
+		document.body.removeChild(s);
+	    }
+
+	    function trigger()
+	    {
+		window.userip = false;
+		var s = document.createElement("script");
+		s.async = true;
+		s.onload = () => response(s);
+		s.onerror = () => response(s);
+		s.src = "https://l2.io/ip.js?var=userip";
+		document.body.appendChild(s);
+	    }
+
+	    if (/^(interactive|complete)$/i.test(document.readyState)) {
+		trigger();
+	    } else {
+		document.addEventListener('DOMContentLoaded', trigger);
+	    }
+	}
+	</script>
 </html>
